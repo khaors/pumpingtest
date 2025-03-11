7d84981e5e40e33e957ef3921f5200b208366c2a
khaors <khaors@gmail.com>
2025-03-11 05:28:29 -0500

    copulasedas removed from other files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // factorial_cpp
 double factorial_cpp(const int& n);
 RcppExport SEXP _pumpingtest_factorial_cpp(SEXP nSEXP) {
